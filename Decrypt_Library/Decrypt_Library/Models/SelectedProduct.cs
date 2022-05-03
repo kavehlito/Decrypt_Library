d4a22029d69ea7701427718783bae26cf41db683
Kavehlito <kajaliliyan@gmail.com>
2022-05-03 12:59:09 +0200

    started implementing product view

---
+++ Decrypt_Library/Decrypt_Library/Models/SelectedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decrypt_Library.Models
+{
+    internal class SelectedProduct
+    {
+        public int Id { get; set; }
+        public string Audience { get; set; }
+        //public string Language { get; set; }
+        public string Category { get; set; }
+        public string Shelf { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string Description { get; set; }
+        public string Narrator { get; set; }
+        public long? Isbn { get; set; }
+        public string Publisher { get; set; }
+        public int? Pages { get; set; }
+        public double? Playtime { get; set; }
+        public DateTime? PublishDate { get; set; }
+        public bool? Status { get; set; }
+    }
+}
---
+++ Decrypt_Library/Decrypt_Library/Views/SelectedProductView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Decrypt_Library.EntityFrameworkCode;
+
+namespace Decrypt_Library.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SelectedProductView : ContentPage
+    {
+        public SelectedProductView()
+        {
+            InitializeComponent();
+        }
+        public SelectedProductView(int selectedId)
+        {
+            InitializeComponent();
+            BindingContext = EntityframeworkProducts.ShowProductInformation(selectedId);
+        }
+    }
+}
