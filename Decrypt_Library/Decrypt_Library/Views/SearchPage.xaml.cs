d460f0ab47ec0fd4be2f3f135a9af71ec2f3b2be
Kavehlito <kajaliliyan@gmail.com>
2022-05-03 15:31:46 +0200

    found missing fågelmås

--- Decrypt_Library/Decrypt_Library/Views/SearchPage.xaml.cs
+++ Decrypt_Library/Decrypt_Library/Views/SearchPage.xaml.cs
@@ -1,25 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Decrypt_Library.EntityFrameworkCode;
+using Decrypt_Library.Models;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Decrypt_Library.EntityFrameworkCode;
-using Decrypt_Library.Models;
-using System.Collections;
 
 namespace Decrypt_Library.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    
+
     public partial class SearchPage : ContentPage
     {
         public SearchPage()
         {
             InitializeComponent();
-            ProductList.ItemsSource = EntityframeworkProducts.ShowAllProducts();
+            //ProductList.ItemsSource = EntityframeworkProducts.ShowAllProducts();
             this.BindingContext = this;
         }
 
@@ -30,16 +24,15 @@ namespace Decrypt_Library.Views
 
         private async void ProductList_ItemTapped(object sender, ItemTappedEventArgs e)
         {
-            var selectedItem = (Product)e.Item;
-            
-            await Navigation.PushAsync(new SelectedProductView(selectedItem.Id));
+            var selectedItemFromList = (CategoryName_Product)e.Item;
+            await Navigation.PushAsync(new SelectedProductView(selectedItemFromList.Id));
             ((ListView)sender).SelectedItem = null;
         }
 
         private void SearchBar_SearchButtonPressed(object sender, EventArgs e)
         {
             var userInput = sender as SearchBar;
-            
+
         }
     }
 }
