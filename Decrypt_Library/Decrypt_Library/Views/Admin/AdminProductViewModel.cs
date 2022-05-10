d037cb815355644eb5762f091c95fd16abcdd6f1
Jonathan <Jonathan@DESKTOP-CKOU825>
2022-05-10 12:23:27 +0200

    product finished and bug free

---
+++ Decrypt_Library/Decrypt_Library/Views/Admin/AdminProductViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Decrypt_Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Decrypt_Library.Views.Admin
+{
+    internal class AdminProductViewModel : BindableObject
+    {
+        private ObservableCollection<Product> _CollectionList;
+        public ObservableCollection<Product> CollectionList 
+        { 
+            get { return _CollectionList; }
+            set
+            {
+                _CollectionList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand DeleteCommand { get; }
+
+
+        public AdminProductViewModel()
+        {
+            CollectionList = new ObservableCollection<Product>(EntityFrameworkCode.EntityframeworkProducts.ShowAllProducts());
+            DeleteCommand = new Command(OnDeleteTapped);
+        }
+
+        public void OnDeleteTapped(object obj)
+        {
+            var content = obj as Product;
+            CollectionList.Remove(content);
+            EntityFrameworkCode.EntityframeworkProducts.RemoveProduct(content);
+            CollectionList = new ObservableCollection<Product>(EntityFrameworkCode.EntityframeworkProducts.ShowAllProducts());
+        }
+
+
+
+    }
+}
