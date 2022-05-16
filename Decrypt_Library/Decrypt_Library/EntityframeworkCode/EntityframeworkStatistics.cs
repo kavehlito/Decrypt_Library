307af39899986299cf65e32a2762a9b435c93f91
Lollo <Lollo@MSI>
2022-05-16 12:00:54 +0200

    statistik brainstorming

--- Decrypt_Library/Decrypt_Library/EntityframeworkCode/EntityframeworkStatistics.cs
+++ Decrypt_Library/Decrypt_Library/EntityframeworkCode/EntityframeworkStatistics.cs
@@ -9,10 +9,14 @@ namespace Decrypt_Library.EntityframeworkCode
         // Skapa metod för att hämta in info från databas och returnera en lista som sedan ska visas DESC, 
         // senast utlånad högst upp
         // Mest frekvent läst kategori - lånehistorik
+        // Mest utlånat inom respektive Medietyp
+        // Antal böcker i sortimentet just nu
+        // Hur många förseningar
+        // Hur många utlånade just nu
+        // Statistik på hur långa reservationer för resp bok ** 
         // Kommer joina Kategorier och Users (skulle vi tro)
-        // Funktion för mest populära event 
+        // Funktion för mest populära event - går ej att testa än
         // TIPS PÅ NY FUNKTION: I statistik ska en knapp finnas "Skicka varning" där när användare loggar in nästa
         // gång så dyker en DisplayAlert upp med "Du har fått en varning, lämna genast tillbaka din bok"
-        // Statistik på hur långa reservationer för resp bok ** 
     }
 }
