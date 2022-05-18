b5f5a719260b96ced7ab5a909df2f7a6e0d9cca5
Kavehlito <kajaliliyan@gmail.com>
2022-05-18 15:45:35 +0200

    implemented review, alpha version

---
+++ Decrypt_Library/Decrypt_Library/Models/ReviewNames.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decrypt_Library.Models
+{
+    internal class ReviewNames
+    {
+        public string UserName { get; set; }
+        public string ReviewText { get; set; }
+        public int? Stars { get; set; }
+    }
+}
