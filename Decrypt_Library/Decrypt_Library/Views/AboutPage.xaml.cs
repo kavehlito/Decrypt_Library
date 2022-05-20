6d0109db2b646c1c1d6fb1fa5a0ac5d0b2ee64eb
Jonathan <Jonathan@DESKTOP-CKOU825>
2022-05-20 16:53:55 +0200

    Aboutpage fixed

--- Decrypt_Library/Decrypt_Library/Views/AboutPage.xaml.cs
+++ Decrypt_Library/Decrypt_Library/Views/AboutPage.xaml.cs
@@ -21,5 +21,39 @@ namespace Decrypt_Library.Views
         {
 
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            contantInfo.IsVisible = false;
+            openTimes.IsVisible = false;
+            aboutUs.IsVisible = true;
+            conplaints.IsVisible = false;
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            contantInfo.IsVisible = false;
+            openTimes.IsVisible = true;
+            conplaints.IsVisible = false;
+            aboutUs.IsVisible = false;
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+            contantInfo.IsVisible = true;
+            openTimes.IsVisible = false;
+            aboutUs.IsVisible = false;
+            conplaints.IsVisible = false;
+
+        }
+
+        private void Button_Clicked_3(object sender, EventArgs e)
+        {
+            contantInfo.IsVisible = false;
+            openTimes.IsVisible = false;
+            aboutUs.IsVisible = false;
+            conplaints.IsVisible = true;
+        }
     }
 }
