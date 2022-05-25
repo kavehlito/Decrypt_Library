e001c77035bc56b0052dce2f4ff062911dbdf724
Kavehlito <kajaliliyan@gmail.com>
2022-05-25 14:55:34 +0200

    fixed if statement for recommended stuff

--- Decrypt_Library/Decrypt_Library/EntityframeworkCode/EntityframeworkUsers.cs
+++ Decrypt_Library/Decrypt_Library/EntityframeworkCode/EntityframeworkUsers.cs
@@ -89,7 +89,7 @@ namespace Decrypt_Library.EntityFrameworkCode
         }
 
         // Recommendation list based on past loans of books and agegroup
-        
+
         public static List<Product> ShowRecommendations()
         {
             var recoList = new List<Product>();
@@ -98,17 +98,17 @@ namespace Decrypt_Library.EntityFrameworkCode
                 var product = db.Products;
                 var recommend = (from
                                 reco in db.BookHistories
-                                join prod in db.Products on reco.ProductId equals prod.Id
-                                join aud in db.Audiences on prod.AudienceId equals aud.Id
-                                join category in db.Categories on prod.CategoryId equals category.Id
-                                where reco.UserId == UserLogin.thisUser.Id
-                                select new MyPagesProductList
-                                {
-                                    AgeGroupId = prod.AudienceId,
-                                    CategoryId = prod.CategoryId,
-                                    UsersID = reco.UserId,
+                                 join prod in db.Products on reco.ProductId equals prod.Id
+                                 join aud in db.Audiences on prod.AudienceId equals aud.Id
+                                 join category in db.Categories on prod.CategoryId equals category.Id
+                                 where reco.UserId == UserLogin.thisUser.Id
+                                 select new MyPagesProductList
+                                 {
+                                     AgeGroupId = prod.AudienceId,
+                                     CategoryId = prod.CategoryId,
+                                     UsersID = reco.UserId,
 
-                                }).ToList();
+                                 }).ToList();
 
                 foreach (var item in recommend)
                 {
@@ -116,34 +116,32 @@ namespace Decrypt_Library.EntityFrameworkCode
                 }
 
             }
-                return recoList.ToList();
+            return recoList.ToList();
         }
-        public static List<MyPagesProductList> ShowTopFiveMostReadNoHistory()
+        public static List<Product> ShowTopFiveMostReadNoHistory()
         {
-           // var topfiveList = new List<Product>();
+            var topfiveList = new List<Product>();
             using (var db = new Decrypt_LibraryContext())
             {
-               // var products = db.Products;
+                var products = db.Products;
 
                 var mostRead = (from
                               mostPop in db.BookHistories
                                 join product in db.Products on mostPop.ProductId equals product.Id
-                                where mostPop.EventId == 2 && mostPop.UserId == UserLogin.thisUser.Id
                                 select product).ToList().GroupBy(c => c.Id)
                                    .Select(c => new MyPagesProductList
                                    {
                                        Title = c.FirstOrDefault().Title,
                                        Count = c.Count(),
-                                   }).OrderByDescending(c => c.Count).ToList().Take(5);
-                /*
+                                   }).OrderByDescending(c => c.Count).ToList();
+
                 foreach (var item in mostRead)
                 {
                     topfiveList = products.Where(p => p.Id == item.ID).ToList();
                 }
-                */
-                return mostRead.ToList();
-            }
 
+                return topfiveList.ToList();
+            }
         }
     }
 }
